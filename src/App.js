4705ecd023a8f62799e51c8b4bb2c4738e7105ff
Ramon Rangel Lima <ramon.lima@radixeng.com.br>
2020-11-12 07:07:47 -0300

    Implemntando comportamento de exclusão e adição de repositórios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(resp => {
+      setRepositories(resp.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const resp = await api.post('repositories', {
+      url: 'https://github.com/lo0seramon/conceitos-nodejs.git',
+      title: `Conceitos NodeJS ${Date.now()}`,
+      techs: ['Express', 'Mongoose', 'Adonis']
+    });
+
+    const repo = resp.data;
+
+    setRepositories([...repositories, repo])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const resp = await api.delete(`repositories/${id}`);
+    
+    if(!resp.status === 204) {
+      return console.log('error');
+    }
+
+    setRepositories(repositories.filter(repo => repo.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repo => 
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
